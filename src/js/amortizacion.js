f039f1f531054b1e2c3c34df68e40743668b67ae
jhseb <jhoanxjt@gmail.com>
2022-12-09 20:00:52 -0500

    prueba

--- src/js/amortizacion.js
+++ src/js/amortizacion.js
@@ -15,15 +15,134 @@ function calcular(){
 	plazo = $("#plazo").val();
 	interes  = $("#tasa").val();
 	//interes = interes/12;
-	tasa = (parseFloat(interes)/100);
+	tasa = (parseFloat(interes));
 	monto = $("#monto").val();
 	saldo = monto;
+	
+
+	tipo_i = document.getElementById("tipo_i").value;
+	tipo_p = document.getElementById("tipo_p").value;
+	tipo_a = document.getElementById("tipo_a").value;
+	mostrar = document.getElementById("tipo_a").value;
+
+
+	tasa = tasa / 100;
+	//Pasar a efectiva sin importar el caso
+	tasa = transformar_interes(tasa, tipo_i, tipo_a);
+	// Comversion de tasas
+	tasa = definir_variables(tasa, tipo_i, plazo, tipo_p);
+
 	calcularCuota();
 	construct();
 	generarTabla();
 }
 
-function calcularCuota(){
+
+function calcular_plazo(plazo, tipo_p) {
+	switch (tipo_p) {
+		case "M":
+			mostrar = "Mensual";
+			break;
+		case "B":
+			mostrar = "Bimestral";
+			break;
+		case "T":
+			mostrar = "Trimestral";
+			break;
+		case "C":
+			mostrar = "Cuatrimestral";
+			break;
+		case "S":
+			mostrar = "Semestral";
+			break;
+		case "A":
+			mostrar = "Anual";
+			break;
+	}
+
+	return plazo;
+
+}
+
+function anticipada(interes, tipo_a) {
+	switch (tipo_a) {
+		case "Si":
+			interes = (interes / (1 - interes));
+			break;
+		case "No":
+			break;
+	}
+	return interes;
+}
+
+function transformar_interes(interes, tipo, tipo_a) {
+	if (tipo == "N_M") {
+		interes = interes / 12;
+	} else if (tipo == "N_B") {
+		interes = interes / 6;
+	} else if (tipo == "N_T") {
+		interes = interes / 4;
+	} else if (tipo == "N_C") {
+		interes = interes / 3;
+	} else if (tipo == "N_S") {
+		interes = interes / 2;
+	} else if (tipo == "N_A") {
+		interes = interes / 1;
+	}
+	interes = anticipada(interes, tipo_a);
+	return interes;
+
+}
+
+function definir_variables(interes, tipo_i, plazo, tipo_p) {
+
+	if (tipo_p == "M") {
+		ele1 = 12;
+	} else if (tipo_p == "B") {
+		ele1 = 6;
+	} else if (tipo_p == "T") {
+		ele1 = 4;
+	}
+	else if (tipo_p == "C") {
+		ele1 = 3;
+	} else if (tipo_p == "S") {
+		ele1 = 2;
+	} else if (tipo_p == "A") {
+		ele1 = 1;
+	}
+
+	if (tipo_i == "Ef_M" || tipo_i == "N_M") {
+		ele2 = 12;
+	} else if (tipo_i == "Ef_B" || tipo_i == "N_B") {
+		ele2 = 6;
+	} else if (tipo_i == "Ef_T" || tipo_i == "N_T") {
+		ele2 = 4;
+	} else if (tipo_i == "Ef_C" || tipo_i == "N_C") {
+		ele2 = 3;
+	} else if (tipo_i == "Ef_S" || tipo_i == "N_S") {
+		ele2 = 2;
+	} else if (tipo_i == "Ef_A" || tipo_i == "N_A") {
+		ele2 = 1;
+	}
+
+	interes = conversion(interes, ele1, ele2);
+	return interes;
+}
+
+
+function conversion(interes, ele1, ele2) {
+	division = (ele2 * (1 / ele1));
+
+	if (ele1 != ele2) {
+		interes = (Math.pow(interes + 1, division));
+		interes = interes - 1;
+
+	}
+	return interes;
+}
+
+function calcularCuota() {
+	alert(tasa);
 	k =  ( 1 - Math.pow((1 + tasa), (plazo*-1))  ) /  tasa;
 	cuota = monto / k;
 	cuota = cuota.toFixed(2);
